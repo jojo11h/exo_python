24455bdac6a913f1743c61ab6ce85ae50a445fd6
jojo11h <148336324+jojo11h@users.noreply.github.com>
2023-10-22 12:32:57 +0200

    Update 06.prime_number.py

--- 06.prime_number.py
+++ 06.prime_number.py
@@ -1,6 +1,10 @@
 def isprime(num):
-    for n in range(2,int(num ** 0.5 ) + 1):
-        if num % n== 0:
+
+    for n in range(2, int(num)):
+        if num % n == 0:
             return False
-    return True
+        else:
+            return True
+
+
 print(isprime(int(input('Donne un chiffre : '))))
